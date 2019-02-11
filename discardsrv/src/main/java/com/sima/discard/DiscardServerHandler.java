e1ed57863f6e8dd1cdd67fc1c2a3b308c04732bb
qiangsima <qsima@seu.edu.cn>
2019-02-11 17:51:40 +0800

    Add Discard server

---
+++ discardsrv/src/main/java/com/sima/discard/DiscardServerHandler.java
@@ -0,0 +1,24 @@
+package com.sima.discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Created by qisima on 2/11/2019 5:16 PM
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        // Discard the received data silently.
+        ((ByteBuf) msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
